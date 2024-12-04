68f6a8ce097898363d100f002f54cbd33699f905
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-12-04 15:44:54 +0800

    Add amica17mkl.exe

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 @echo off
-mpif90 -fc=ifx /Qopenmp /Qmkl /F67108864 /DMKL /fpp /O3 /QxHost /Qipo /fp:precise /exe:amica15mkl.exe funmod2.f90 amica15.f90
+mpif90 -fc=ifx /Qopenmp /Qmkl /F134217728 /DMKL /fpp /O3 /QxHost /Qipo /fp:precise /exe:amica15mkl.exe funmod2.f90 amica15.f90
+mpif90 -fc=ifx /Qopenmp /Qmkl /F134217728 /DMKL /fpp /O3 /QxHost /Qipo /fp:precise /exe:amica17mkl.exe funmod2.f90 amica17.f90
